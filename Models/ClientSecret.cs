1bdd53593be8af9ab8ba28d7383674e44dc4ad88
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-01-28 10:38:59 +0530

    Implement the model class for ClientServer and Employee.
    Add the package referrance for EntityFramework SqlServer and Tools

---
+++ Models/ClientSecret.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations
+namespace HMACServerApp.Models
+{
+    public class ClientSecret
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string ClientId { get; set; } = null!;
+
+        [Required]
+        [MaxLength(200)]
+        public string SecretKey { get; set; } = null!;
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HMACServerApp.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;   
+
+        [Required]
+        [MaxLength(50)]
+        public string Position { get; set; } = null!;
+         
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Salary { get; set; }
+    }
+}
